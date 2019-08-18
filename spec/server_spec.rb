42651a5dce2d470285a056b2c1a634ca5c24dbfd
Antoine Carat <antoine@browserstack.com>
2019-08-18 12:15:17 +0100

    :white_check_mark: Add minimal test for root (hello world)

--- spec/server_spec.rb
+++ spec/server_spec.rb
@@ -1,5 +1,16 @@
-require_relative "../server"
+require 'rspec'
+require 'rack/test'
+
+require_relative './spec_helper'
+require_relative '../server'
 
 RSpec.describe PizzaPlanetServer do
+  describe 'GET /' do
+    before { get '/' }
 
+    it 'returns Hello, world! (200)' do
+      expect(last_response.status).to eq 200
+      expect(last_response.body).to eq('Hello, world!')
+    end
+  end
 end
