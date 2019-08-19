70b6194efe69a4fa56fd1e66be92f8948cbae06c
Antoine Carat <antoine@browserstack.com>
2019-08-19 09:00:18 +0100

    Add tests for / and /pizzas

--- spec/server_spec.rb
+++ spec/server_spec.rb
@@ -8,9 +8,37 @@ RSpec.describe PizzaPlanetServer do
   describe 'GET /' do
     before { get '/' }
 
-    it 'returns Hello, world! (200)' do
+    it 'respond with code 200' do
       expect(last_response.status).to eq 200
-      expect(last_response.body).to eq('Hello, world!')
+    end
+  end
+
+  describe 'GET /pizzas' do
+    before { get '/pizzas' }
+
+    it 'should respond with code 200' do
+      expect(last_response.status).to eq 200
+    end
+
+    it 'returns an Array of pizzas' do
+      expect(JSON.parse(last_response.body)).to be_an Array
+    end
+  end
+
+  describe 'GET /order/:ordernumber' do
+    context 'when the order is in DB' do
+      before { get '/order/45332' }
+
+      it 'should respond with code 200' do
+        expect(last_response.status).to eq 200
+      end
+
+      it 'returns a hash containing an array of pizzas and a price' do
+        expect(JSON.parse(last_response.body)).to be_a Hash
+        expect(JSON.parse(last_response.body)['pizzas']).to be_an Array
+        expect(JSON.parse(last_response.body)['total']).to be_a Number
+
+      end
     end
   end
 end
