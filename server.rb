8d986189b6715a86d5bf777248f4aeb2c0567462
Antoine Carat <antoine@browserstack.com>
2019-08-19 08:59:27 +0100

    Add frontend

--- server.rb
+++ server.rb
@@ -1,5 +1,28 @@
 require 'sinatra'
+require 'sinatra/cross_origin'
+require 'json'
 
+# PizzaPlanetServer is awesome
 class PizzaPlanetServer < Sinatra::Base
+  set :bind, '0.0.0.0'
 
+  configure do
+    enable :cross_origin
+  end
+
+  before do
+    response.headers['Access-Control-Allow-Origin'] = '*'
+  end
+
+  get '/' do
+    status 200
+    # 'Hello, world!'
+    File.read(File.join('public', 'index.html'))
+  end
+
+  get '/pizzas' do
+    status 200
+    db = JSON.parse(File.read('db.json'))
+    db['pizzas'].to_json
+  end
 end
